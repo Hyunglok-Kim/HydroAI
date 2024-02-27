4a29be8afd52c741150e138be70871748c64ca50
SubinKim-phy <101175347+SubinKim-phy@users.noreply.github.com>
2024-02-27 10:18:45 +0900

    add file via upload for SIEB class

---
+++ check_import.py
@@ -0,0 +1,8 @@
+print("HydroAI libraries are import successfully!")
+print("You can import other python package files (*.py) in hydroAI folder.")
+
+
+def message():
+    print("This is < message > function in check_import.py file")
+
+
