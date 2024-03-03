151186dd286cbd493f7c2233a959be824ba8f36b
Hyung <hyunglokkim@gmail.com>
2024-03-03 22:01:33 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# The __init__.py file is used to initialize a Python package and can also be used to specify what objects the package exports,
+# run initialization code for the package, or define variables. 
+# However, in many cases, especially when we are simply organizing your modules into packages without needing any package-level
+# initialization or configuration, the __init__.py file can be completely empty.
