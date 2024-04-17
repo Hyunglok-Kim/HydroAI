39ddbdc00d9c20a34aeb59830c9f38343c62a9ca
hyunglok <hyunglokkim@gmail.com>
2024-04-17 23:01:25 +0900

    example lsm updated

--- Plot.py
+++ Plot.py
@@ -459,7 +459,7 @@ def plot_LULC_map_MCD12C1(longitude, latitude, values, lulc_type=1, title='MCD12
     # Create legend patches for detailed legend
     legend_patches = [mpatches.Patch(color=cmap_colors[i], label=labels[i]) for i in range(len(labels))]
     # Display the legend in 4 columns as requested
-    legend = ax.legend(handles=legend_patches, title='Land Cover Classes', loc='upper center', bbox_to_anchor=(0.5, -0.1), fancybox=True, shadow=False, ncol=3)
+    legend = ax.legend(handles=legend_patches, title='Land Cover Classes', loc='upper center', bbox_to_anchor=(0.5, -0.02), fancybox=True, shadow=False, ncol=3)
 
     plt.show()
 
